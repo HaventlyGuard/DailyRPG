74e9110d4ce3810b18efe993cbc3231a5415f0e1
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-06-03 16:40:05 +0700

    Интерфейсы

---
+++ identity-service/Features/Users/Repository/IUserRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace identity_service.Features.User.Repository;
+
+public interface IUserRepository
+{
+    public Task<User> CreateUser(User user);
+    public Task<User> GetUserByEmail(string email);
+    public Task<User> GetUserBySub(string sub);
+}
---
+++ identity-service/Features/Users/Service/IUserService.cs
@@ -0,0 +1,7 @@
+﻿namespace identity_service.Features.User;
+
+public interface IUserService
+{
+    public Task<User> GetOrCreateUser();
+    public Task<User> GetUserBySub(string sub);
+}
