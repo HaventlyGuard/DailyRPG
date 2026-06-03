9f6ae3337989f5ecab5f4381a13bfbc36074d2a9
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-06-03 16:40:27 +0700

    Сервис и репозиторий

---
+++ identity-service/Features/Users/Service/UserService.cs
@@ -0,0 +1,17 @@
+﻿namespace identity_service.Features.User;
+
+public class UserService : IUserService
+{
+    private IHttpContextAccessor _accessor;
+    
+    
+    public async Task<User> GetOrCreateUser()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<User> GetUserBySub(string sub)
+    {
+        throw new NotImplementedException();
+    }
+}
