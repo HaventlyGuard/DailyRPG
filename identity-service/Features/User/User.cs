60798c0328a9126f11339824c85a5b08ce4216e1
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-06-03 14:42:22 +0700

    Модель юзера

---
+++ identity-service/Features/User/User.cs
@@ -0,0 +1,13 @@
+﻿namespace identity_service.Features.User;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string KeycloakSub { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string DisplayName { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+    public DateTime LastLoginAt { get; set; }
+    public bool IsActive { get; set; } = true;
+}
