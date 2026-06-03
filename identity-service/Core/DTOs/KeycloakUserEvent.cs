f6144aa44b4428c0c368f99378dd0c6014ee80be
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-06-03 14:42:33 +0700

    Дто для кейклока

---
+++ identity-service/Core/DTOs/KeycloakUserEvent.cs
@@ -0,0 +1,8 @@
+﻿public class KeycloakUserEvent
+{
+    public string UserId { get; set; } = "";
+    public string Username { get; set; } = "";
+    public string Email { get; set; } = "";
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+}
