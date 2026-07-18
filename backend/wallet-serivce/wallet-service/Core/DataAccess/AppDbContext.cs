0405ed6e9325e81cd9b9c004d3a6f294dba205be
Lyamin Timofey <162110108+HaventlyGuard@users.noreply.github.com>
2026-07-18 19:59:49 +0700

    Добавил дбконтекст и докер

---
+++ backend/wallet-serivce/wallet-service/Core/DataAccess/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using wallet_service.Features.Transaction;
+using wallet_service.Features.Wallet;
+
+namespace wallet_service.Core.DataAccess;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    
+    public DbSet<Wallet> Wallets => Set<Wallet>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Wallet>(entity =>
+        {
+            entity.ToTable("Wallets");
+            entity.HasKey(e => e.Id);
+            entity.HasIndex(e => e.UserId).IsUnique();
+        });
+
+        modelBuilder.Entity<Transaction>(entity =>
+        {
+            entity.ToTable("Transactions");
+            entity.HasKey(e => e.Id);
+            entity.HasIndex(e => e.WalletId);
+            entity.HasIndex(e => e.CreatedAt);
+            entity.HasOne(e => e.Wallet).WithMany().HasForeignKey(e => e.WalletId);
+        
+            entity.Property(e => e.CurrencyType).HasConversion<string>().HasMaxLength(20);
+            entity.Property(e => e.Type).HasConversion<string>().HasMaxLength(20);
+        });
+    }
+}
